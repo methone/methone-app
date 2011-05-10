1daf3aec732f44530a55f6197acea159458aa12e
thiagoprocaci <thiagoprocaci@gmail.com>
2011-05-09 22:30:05 -0300

    testes de controller de parceiro

--- grails-app/views/parceiro/create.gsp
+++ grails-app/views/parceiro/create.gsp
@@ -6,7 +6,7 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <meta name="layout" content="main" />
         <g:set var="entityName" value="${message(code: 'parceiro.label', default: 'Parceiro')}" />
-        <title><g:message code="default.create.label" args="[entityName]" /></title>
+        <title><g:message code="cadastroParceiro"  /></title>
     </head>
     <body>
         <div class="nav">
@@ -14,7 +14,7 @@
             <span class="menuButton"><g:link class="list" action="list"><g:message code="default.list.label" args="[entityName]" /></g:link></span>
         </div>
         <div class="body">
-            <h1><g:message code="default.create.label" args="[entityName]" /></h1>
+            <h1><g:message code="cadastroParceiro" /></h1>
             <g:if test="${flash.message}">
             <div class="message">${flash.message}</div>
             </g:if>
@@ -125,7 +125,9 @@
                     </table>
                 </div>
                 <div class="buttons">
-                    <span class="button"><g:submitButton name="create" class="save" value="${message(code: 'default.button.create.label', default: 'Create')}" /></span>
+                    <span class="button">
+                     <g:submitButton name="create" class="save" value="${message(code: 'default.button.save.label', default: 'Create')}" />
+                    </span>
                 </div>
             </g:form>
         </div>
--- test/unit/com/methone/ParceiroControllerTests.groovy
+++ test/unit/com/methone/ParceiroControllerTests.groovy
@@ -2,6 +2,8 @@ package com.methone
 
 import grails.test.*
 
+import com.methone.enumeration.Interesse
+
 class ParceiroControllerTests extends ControllerUnitTestCase {
 
 	protected void setUp() {
@@ -13,9 +15,40 @@ class ParceiroControllerTests extends ControllerUnitTestCase {
 	}
 
 	void testCreate() {
-		Map myParams = [login : 'login', email : 'teste@teste.com']
-        controller.params.putAll(myParams)
 		def map = controller.create()
 		assertNotNull map
+		assertEquals  1, map.size()
+		assertNotNull map.parceiroInstance
+		assertTrue map.parceiroInstance instanceof Parceiro
+		assertNull  map.parceiroInstance.version
+		assertNull  map.parceiroInstance.id
 	}
+
+	void testSaveErro(){
+		// mock das instancias de parceiro.
+		// adiciona os metodos gerados dinamicamente
+		mockDomain(Parceiro)
+		controller.save()
+		assertEquals "create", renderArgs.view
+		assertNotNull renderArgs.model
+		assertNotNull renderArgs.model.parceiroInstance
+		assertTrue renderArgs.model.parceiroInstance instanceof Parceiro
+
+	}
+
+	void testSaveSucesso(){
+		//  mocks do params para instanciar o parceiro no save
+		def paramsMock = [login : 'login', email : 'teste@teste.com', senha: "senha",
+				nome:"nome", telefone: "telefone", endereco : "endereco",
+				cep : "cep", estado: "estado", cidade : "cidade", interesse: "AMBOS"]
+		controller.params.putAll(paramsMock)
+		mockDomain(Parceiro)
+		controller.metaClass.message = {args -> println "${args}"}
+		controller.save()
+		assertNotNull redirectArgs
+		assertNotNull redirectArgs.action
+		assertEquals "show", redirectArgs.action
+		assertNotNull redirectArgs.id
+	}
+
 }
